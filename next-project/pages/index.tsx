9f2d703cb91aabef8ef682fcf198908169b2d2d6
Christian Dela Peña <dev.chris.dp@gmail.com>
2022-10-18 07:58:55 +0800

    With Static Export Html

--- next-project/pages/index.tsx
+++ next-project/pages/index.tsx
@@ -2,17 +2,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 const remoteCVImageSource =
-  "https://lh3.googleusercontent.com/drive-viewer/AJc5JmT4UL55YXUeMTw4Xh8FBlVHBmT92gUu7AAStDhdTMEZRVcBwVx6ib9Gibb94DpbT1qZBsqCX8c=w1920-h895";
+  "https://raw.githubusercontent.com/ProteanDev/ProteanDev.github.io/master/cv-img.png";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-start">
       <Head>
         <title>Christian Dela Peña</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
+      <main className="flex w-full flex-col items-center justify-center text-center">
         <>
           <picture className="w-full">
             <source srcSet={remoteCVImageSource} type="image/png" />
@@ -24,10 +24,6 @@ const Home: NextPage = () => {
           </picture>
         </>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        Christian Dela Peña ( All Rights Received 2022 )
-      </footer>
     </div>
   );
 };
